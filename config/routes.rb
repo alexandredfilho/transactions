2477afd23a14c387051b93a5b75005ed93824645
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2022-10-19 00:27:05 -0300

    Update routes after deploying devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   resources :cnab_transactions, only: [:index, :new, :create]
   resources :stores, only: [:index]
 
-  root 'stores#index'
+  root to: "home#index"
 end

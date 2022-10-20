69b62070dccbd4ed9a78e46723c86e9bca3932fd
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2022-10-19 22:02:32 -0300

    Create a simple dashboard on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @imported_stores = Store.all
+    @imported_transactions = CnabTransaction.all
   end
 end
